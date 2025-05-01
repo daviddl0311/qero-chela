0c3b84638cb51e5c4cd5db503776c988902ffbab
daviddl0311 <daviddominguezladera@gmail.com>
2025-04-30 20:10:47 -0500

    update

--- js/verificado.js
+++ js/verificado.js
@@ -1,5 +1,5 @@
 console.log(localStorage.getItem("verificado"));
 
 if(localStorage.getItem("verificado") == "false" || localStorage.getItem("verificado") == null) {
-    window.location.replace("verificador.html");
+    window.location.replace("https://qerochela.com/verificador.html");
 }
--- js/verificador.js
+++ js/verificador.js
@@ -2,18 +2,18 @@ let button = document.getElementById("btn");
 let button2 = document.getElementById("btn2");
 
 if(localStorage.getItem("verificado") === "true") {
-    window.location.replace("index.html");
+    window.location.replace("https://qerochela.com/");
 } else {
     localStorage.setItem("verificado", "false");
 }
 
 button.addEventListener("click", () => {
     localStorage.setItem("verificado", "true");
-    window.location.replace("index.html");
+    window.location.replace("https://qerochela.com/");
 });
 
 console.log(localStorage.getItem("verificado"));
 
 button2.addEventListener("click", () => {
-    
+    window.location.replace("https://www.google.com/");
 });

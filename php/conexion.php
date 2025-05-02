607cdf6078ea6f69d3499000ef191e4b657c84b8
daviddl0311 <daviddominguezladera@gmail.com>
2025-05-02 08:41:32 -0500

    new

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,7 @@
 <?php
     $conexion = mysqli_connect("localhost", "inalcorp_user_qerochela", "VXp0UUM7VPz_", "inalcorp_qerochela");
+    // $conexion = mysqli_connect("localhost", "root", "", "inalcorp_qerochela");
+    
     if(!$conexion){
         die("Error de conexión: " . mysqli_connect_error());
     }
